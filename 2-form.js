27b10300cd79890fa404f2fe806bf20f38e9a6ad
KaterynaSlobodianiuk <143430452+KaterynaSlobodianiuk@users.noreply.github.com>
2025-02-04 11:34:13 +0000

    Deploying to gh-pages from @ KaterynaSlobodianiuk/goit-js-hw-09@6547d41d4feab1f47d79a8835ac2a6d34f437ebc 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const a={email:"${email.value}",message:"${message.velue}"};document.querySelector(".email-js"),document.querySelector(".message-js");email.addEventListener("input",e);message.addEventListener("input",e);function e(){console.log(a)}
+//# sourceMappingURL=2-form.js.map
