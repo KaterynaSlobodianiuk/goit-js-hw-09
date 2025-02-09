423ac44afa54682072e3fcf60d72929c99983f2a
KaterynaSlobodianiuk <kat.luchko@gmail.com>
2025-02-09 18:49:22 +0200

    changed form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,20 +7,66 @@
 //     показуй сповіщення з текстом «Fill please all fields». Якщо всі поля заповнені, виведи у консоль об’єкт formData з актуальними значеннями,
 //         очисти локальне сховище, об’єкт formData і поля форми.
 
+const storageKey = 'feedback-form-state';
+
 const formData = {
-    email: "${email.value}",
-    message: "${message.velue}",
+  email: '',
+  message: '',
 };
 
 const refs = {
-    email: document.querySelector(`.email-js`),
-    message: document.querySelector(`.message-js`),
+  form: document.querySelector('form'),
+  email: document.querySelector('.email-js'),
+  message: document.querySelector('.message-js'),
+};
+
+function initPage() {
+  const localData = loadFromLS(storageKey);
+  if (localData) {
+    refs.email.value = localData.email || '';
+    refs.message.value = localData.message || '';
+  }
 }
 
-email.addEventListener(`input`, onInputChange);
-message.addEventListener(`input`, onInputChange);
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const body = localStorage.getItem(key);
+  try {
+    return JSON.parse(body);
+  } catch {
+    return body;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const emailForm = e.target.elements.email.value.trim();
+  const messageForm = e.target.elements.message.value.trim();
+  if (!emailForm || !messageForm) {
+    alert('Fill please all fields');
+    return;
+  }
+  formData.email = emailForm;
+  formData.message = messageForm;
+  saveToLS(storageKey, formData);
+  console.log(formData);
+  localStorage.removeItem(storageKey);
+  e.target.reset();
+}
 
 function onInputChange() {
-    console.log(formData);
+  formData.email = refs.email.value.trim();
+  formData.message = refs.message.value.trim();
+  saveToLS(storageKey, formData); 
 }
 
+initPage();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.email.addEventListener('input', onInputChange);
+refs.message.addEventListener('input', onInputChange);
